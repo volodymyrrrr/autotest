b11f832fc4fe6107275b3e377711457b87140f65
volodymyrrrr <125571431+volodymyrrrr@users.noreply.github.com>
2023-02-17 12:22:47 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def driver():
     driver_service = Service()
     options = webdriver.ChromeOptions()
     options.add_argument("--incognito")
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     driver = webdriver.Chrome(service=driver_service, options=options)
     driver.maximize_window()
     driver.delete_all_cookies()
